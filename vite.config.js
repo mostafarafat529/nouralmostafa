ab4c043f2b035ea0ea347860da8f20fdfa288971
mostafa rafat <mostafarafat@gmail.com>
2026-06-14 17:15:21 +0200

    add the pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nour Al Mustafa',
+        short_name: 'Nour',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
-
